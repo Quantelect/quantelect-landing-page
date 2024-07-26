1c7e16fe320e2d66447512ea8cac5208e72c7f51
Ubaidullah Shuaib <137732525+ubaidullahshuaib@users.noreply.github.com>
2024-07-26 09:05:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {  
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {  
+    anchor.addEventListener('click', function(e) {  
+      e.preventDefault();  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({  
+        behavior: 'smooth'  
+      });  
+    });  
+  });  
+  
   document.getElementById('contact-form').addEventListener('submit', function(e) {  
     e.preventDefault();  
   
