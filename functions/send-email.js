fbc996cb5a7666a4247c7ce2fbe3f11f2a169b70
Ubaidullah Shuaib <137732525+ubaidullahshuaib@users.noreply.github.com>
2024-07-26 06:44:00 +0100

    Add files via upload

---
+++ functions/send-email.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer');  
+  
+exports.handler = async (event, context) => {  
+  if (event.httpMethod !== "POST") {  
+    return {  
+      statusCode: 405,  
+      body: JSON.stringify({ status: 'forbidden' }),  
+    };  
+  }  
+  
+  const data = JSON.parse(event.body);  
+  const { name, email, message } = data;  
+  
+  if (!name || !email || !message) {  
+    return {  
+      statusCode: 400,  
+      body: JSON.stringify({ status: 'validation_error' }),  
+    };  
+  }  
+  
+  const transporter = nodemailer.createTransport({  
+    service: 'ProtonMail',  
+    auth: {  
+      user: process.env.PROTONMAIL_USER,  
+      pass: process.env.PROTONMAIL_PASS,  
+    },  
+  });  
+  
+  const mailOptions = {  
+    from: `${name} <${email}>`,  
+    to: 'info@quantelect.com',  
+    subject: `New Contact Form Submission from ${name}`,  
+    text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,  
+  };  
+  
+  try {  
+    await transporter.sendMail(mailOptions);  
+    return {  
+      statusCode: 200,  
+      body: JSON.stringify({ status: 'success' }),  
+    };  
+  } catch (error) {  
+    return {  
+      statusCode: 500,  
+      body: JSON.stringify({ status: 'error' }),  
+    };  
+  }  
+};  
